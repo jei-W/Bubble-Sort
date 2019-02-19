54c8be7060720051245c74cabf3b97102f5d929b
jei-W <gkdtkdv123@naver.com>
2019-02-19 23:51:10 +0900

    Bubble Sort의 구현 == *오름차순 정렬 구현 *내림차순 정렬 구현

---
+++ Bubble Sort/BubbleSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class BubbleSort
+    {
+        public static void AscendingSort( int[] arg)
+        {
+            int repeat = arg.Length;
+
+            for( int i = 0; i < arg.Length - 1; i++ )
+            {
+                for( int j = 0; j < repeat - 1; j++ )
+                {
+                    if( arg[j] > arg[j + 1] )
+                    {
+                        Swap(ref arg[j], ref arg[j + 1]);
+                    }
+                }
+                --repeat;
+            }
+        }
+
+        public static void DescendingSort( int[] arg )
+        {
+            int repeat = arg.Length;
+
+            for( int i = 0; i < arg.Length - 1; i++ )
+            {
+                for( int j = 0; j < repeat - 1; j++ )
+                {
+                    if( arg[j] < arg[j + 1] )
+                    {
+                        Swap(ref arg[j], ref arg[j + 1]);
+                    }
+                }
+                --repeat;
+            }
+        }
+
+
+        static void Swap( ref int a, ref int b )
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ Bubble Sort/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            int[] arg = { 3, 7, 5, 8, 9, 11, 4, 8 };
+            Print();
+            Console.WriteLine("\n");
+
+            BubbleSort.AscendingSort(arg);
+            Print();
+            Console.WriteLine("\n");
+
+            BubbleSort.DescendingSort(arg);
+            Print();
+            Console.WriteLine("\n");
+
+
+            void Print()
+            {
+                foreach( int i in arg )
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
